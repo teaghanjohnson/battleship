d024f0568b1dc64fef73bb111e75cacb6c28ffa9
teaghanjohnson <mylesjohnson2405@gmail.com>
2025-09-18 00:17:45 -0400

    added a place ship function with shipIds to track individual ships

--- battleship.js
+++ battleship.js
@@ -1,9 +1,13 @@
 // Ship class
 class Battleship {
-  constructor(length) {
+  static nextId = 1;
+
+  constructor(length, direction = "horizontal") {
+    this.id = Battleship.nextId++;
     this.length = length;
     this.hitCount = 0;
     this._isSunk = false;
+    this.direction = direction;
   }
 
   hit() {
@@ -26,9 +30,11 @@ class Battleship {
 }
 
 class GameBoard {
-  constructor(size = 10) {
+  constructor(size = 10, ships = []) {
     this.size = size;
     this.grid = this.createEmptyGrid();
+    // 0 = empty, 1 = ship A, 2 = ship B, etc.
+    this._ships = ships;
   }
 
   createEmptyGrid() {
@@ -38,10 +44,40 @@ class GameBoard {
     // 2's will be misses
     // 3's will be hits
     // may get confusing may optimize later
-    const board = [];
-    for (i = 0; i <= 10; i++) {
-      for (j = 0; j <= 10; j++) {
-        board.append([i][j]);
+    const board = Array(10)
+      .fill(0)
+      .map(() => Array(10).fill(0));
+  }
+
+  placeShip(row, col, battleship) {
+    // battleship is a ship oject
+    // row and col will be front of the ship
+    const board = this.grid;
+
+    board[row][col] = 1;
+    let vertLength = row + 1;
+    let horLength = col + 1;
+
+    if (battleship.direction == "vertical") {
+      // place ship ship.length down
+      // increment exising row value until length reached
+      // if goes over board thow an error not allowing them to place it
+      if (battleship.length + row > 10) {
+        throw Error("place ship on board.");
+      }
+
+      for (vertLength; vertLength < row + battleship.length; vertLength++) {
+        board[vertLength][col] = id;
+      }
+      ships.push([shipId * vertLength]);
+    }
+    if (battleship.direction == "horizontal") {
+      if (battleship.length + col > 10) {
+        throw Error("place ship on board.");
+      }
+      for (horLength; horLength < row + battleship.length; horLength++) {
+        board[vertLength][col] = id;
+        ships.push([shipId * vertLength]);
       }
     }
   }
